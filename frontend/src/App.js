cc42df743d44655b5b4159cd844a1b8b440fb601
flexcommits <andersensven.star@gmail.com>
2020-06-16 23:15:00 +0200

    Added frontend login logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import Login from './Login';
+import Home from './Home';
 import './App.scss';
 
 class App extends Component {
-
+  constructor(props) {
+    super();
+    axios.defaults.baseURL = 'http://localhost:4667/api';
+  }
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Login} />
-          <Route exact path='/' component={Login} />
+          <Route exact path='/home' component={Home} />
         </Switch>
       </BrowserRouter>
     );
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,37 +2,59 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Login.scss';
 import { Button, Form, Alert } from 'react-bootstrap';
+// import { useHistory } from "react-router-dom";
 
 class Login extends Component {
+    // handleClick = e => {
+    //     this.props.history.push("/home");
+    // };
+
 
     state = {
-        persons: [],
+        email: '',
+        password: '',
+        errorMessage: '',
         isLoginError: false
     }
 
-    loginClicked() {
+    loginClicked(history) {
         this.setState({ isLoginError: false });
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
+
+        let context = this;
+        axios.post('/login', {
+            email: this.state.email,
+            password: this.state.password
+        })
             .then(res => {
-                const multiblePersons = res.data;
-                this.setState({ persons: multiblePersons, isLoginError: true });
-                console.log(this.state.persons);
+                history.push('/home');
+            })
+            .catch(function (error) {
+                context.setState({ errorMessage: error.response.data.errorMessage });
             })
     }
 
+    updateStateEmail = e => {
+        this.setState({ email: e.target.value });
+    };
+
+    updateStatePassword = e => {
+        this.setState({ password: e.target.value });
+    };
+
     render() {
+        const { history } = this.props;
         return (
             <div className="container-centered">
                 <div className="card-style-container">
                     <h2>Login</h2>
-                    <Form.Control id="email" aria-describedby="email" type="email" placeholder="Enter email" />
-                    <Form.Control id="password" aria-describedby="email" type="password" placeholder="Enter password" />
-                    {this.state.isLoginError ?
+                    <Form.Control id="email" onChange={this.updateStateEmail} aria-describedby="email" type="email" placeholder="Enter email" />
+                    <Form.Control id="password" onChange={this.updateStatePassword} aria-describedby="email" type="password" placeholder="Enter password" />
+                    {this.state.errorMessage ?
                         <Alert className="username-password-incorrect" variant="danger">
-                            Sorry, username or password was not correct!
-                    </Alert> : null}
+                            {this.state.errorMessage}
+                        </Alert> : null}
 
-                    <Button variant="primary" onClick={() => this.loginClicked()}>Login</Button>
+                    <Button variant="primary" onClick={() => this.loginClicked(history)}>Login</Button>
                 </div>
             </div>
         );
