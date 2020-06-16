e13a1b6996151219e637094af37b139327d6a74f
flexcommits <andersensven.star@gmail.com>
2020-06-16 15:25:49 +0200

    Added backend to project

---
+++ backend/src/main/java/com/interview/cvproject/CvProjectApplication.java
@@ -0,0 +1,13 @@
+package com.interview.cvproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/interview/cvproject/CvProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.cvproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
