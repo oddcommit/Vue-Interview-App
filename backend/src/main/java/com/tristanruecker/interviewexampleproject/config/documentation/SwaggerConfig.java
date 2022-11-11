a035ab7a5d12edfc43217389d54841990f3822a6
flexcommits <andersensven.star@gmail.com>
2022-11-11 21:00:22 +0100

    Add new jjwt project to maven and remove not needed annotation for swagger

--- backend/src/main/java/com/tristanruecker/interviewexampleproject/config/documentation/SwaggerConfig.java
+++ backend/src/main/java/com/tristanruecker/interviewexampleproject/config/documentation/SwaggerConfig.java
@@ -3,19 +3,16 @@ package com.tristanruecker.interviewexampleproject.config.documentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.util.Predicates;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Access via link: <a href="http://localhost:4667/swagger-ui/#/">...</a>
  */
 @Configuration
 @Profile("development")
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
