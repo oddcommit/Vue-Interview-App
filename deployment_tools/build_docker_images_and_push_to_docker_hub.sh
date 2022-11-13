c9588d38768a18e90961e3e20ead9453d0083ebc
flexcommits <andersensven.star@gmail.com>
2022-11-13 18:28:28 +0100

    Correct push to docker hub script

--- deployment_tools/build_docker_images_and_push_to_docker_hub.sh
+++ deployment_tools/build_docker_images_and_push_to_docker_hub.sh
@@ -1,66 +1,67 @@
 # Normally this script is running on an jenkins server or somewhere else
-set -e
-trap "cleanup" ERR
+echo "Starting to build docker container releases and push them to docker hub..."
 
-pomFile=pom.xml
-backendDockerFile=../backend/Dockerfile
+backendTagName=interview-example-project-backend
+frontendTagName=interview-example-project-frontend
+
+relDir="$(dirname -- "$0"; )";
+cd "$relDir" || exit 1;
+scriptDirectory="$( pwd; )";
+
+pomFile=$scriptDirectory/../backend/pom.xml
+backendDockerFile=$scriptDirectory/../backend/Dockerfile
+
+projectVersion="$(cat "$pomFile" | grep "<project.version>" | grep -Eo "[0-9]+.[0-9]+.[0-9]+")"
+echo "Backend project version is $projectVersion"
 
 replaceText() {
     # Mac got a slightly different SED (FreeBSD sed)
     if [[ "$(uname)" == "Darwin" ]]; then
-        sed -i '' "s/$1/$2/g" $3
+        sed -i '' "s/$1/$2/g" "$3"
     else
-        sed -i "s/$1/$2/g" $3
+        sed -i "s/$1/$2/g" "$3"
     fi
+}
+
+
+build_backend_maven_project() {
+    echo "Prepare 'pom.xml' file for production..."
+    replaceText "<release.type>SNAPSHOT" "<release.type>RELEASE" "$pomFile"
+    replaceText "spring.profiles.active=development" "spring.profiles.active=kubernetes" "$pomFile"
+    echo "Trying to create a successfull maven build..."
+
+    cd "$scriptDirectory"/../backend || exit 1
+    ./mvnw clean install
+    echo "Maven build successful!"
+    cd "$scriptDirectory" || exit 1
+}
+
+create_docker_file() {
+      cd "$scriptDirectory"/../backend || exit 1
+      echo "Prepare Dockerfile for production..."
+      replaceText "{project_version}" "$projectVersion" "$backendDockerFile"
+      echo "Dockerfile preparation successful!"
+
+      echo "Building docker image..."
+      docker build --tag $backendTagName:"$projectVersion" --tag $backendTagName:latest .
+      echo "Docker image build successful..."
+      echo "Pushing image to docker hub..."
+      docker push 21321321421441/$backendTagName:"$projectVersion"
+      echo "Push successful"
+      cd "$scriptDirectory" || exit 1
 }
 
 cleanup() {
     echo "cleanup started..."
-    currentDirectory="$(pwd)"
-
-    if [[ $string == *"backend"* ]]; then
-        cd ../deployment_tools
-    fi
-
-    replaceText "<release.type>RELEASE" "<release.type>SNAPSHOT" ../backend/$pomFile
-    replaceText "spring.profiles.active=kubernetes" "spring.profiles.active=development" ../backend/$pomFile
-    replaceText "$projectVersion" '$(projectVersion)' ../backend/$backendDockerFile
+    replaceText "<release.type>RELEASE" "<release.type>SNAPSHOT" "$pomFile"
+    replaceText "spring.profiles.active=kubernetes" "spring.profiles.active=development" "$pomFile"
+    replaceText "$projectVersion" "{project_version}" "$backendDockerFile"
     echo "cleanup ended..."
 }
 
-projectVersion="$(cat ../backend/$pomFile | grep '<project.version>' | grep -Eo '[0-9]+.[0-9]+.[0-9]+')"
-
-backendTagName=21321321421441/interview-example-project-backend
-frontendTagName=21321321421441/interview-example-project-frontend
-
-echo "Starting to build docker container releases and push them to docker hub"
-docker info &>/dev/null
-
-build_backend_docker_container() {
-    echo "Prepare 'pom.xml' file for production..."
-    replaceText "<release.type>SNAPSHOT" "<release.type>RELEASE" $pomFile
-    replaceText "spring.profiles.active=development" "spring.profiles.active=kubernetes" $pomFile
-    echo "Trying to create a successfull maven build..."
-    mvn clean install
-    echo "Maven build successful!"
-    echo "Prepare Dockerfile for production..."
-    projectVersion="$(cat $pomFile | grep '<project.version>' | grep -Eo '[0-9]+.[0-9]+.[0-9]+')"
-    replaceText '$(projectVersion)' "$projectVersion" $backendDockerFile
-    echo "Dockerfile preparation successful!"
-    echo "Building docker image..."
-    docker build --tag $backendTagName:$projectVersion --tag $backendTagName:latest .
-    echo "Docker image build successful..."
-    echo "Pushing image to docker hub..."
-    docker push 21321321421441/$backendTagName:$projectVersion
-    echo "Push successful"
-
-    # echo "Prepareing kubernetes backend yaml file..."
-    # TODO: This has to be implemented
-}
-
-cd ../backend
-build_backend_docker_container
-
+build_backend_maven_project
+create_docker_file
 cleanup
 
-echo "End building container."
+
+echo "Build script end."
