c2d20695d15ca16786fbe0bf07f02d486ef5234b
flexcommits <andersensven.star@gmail.com>
2022-11-20 21:29:57 +0100

    Correct urls for production and correct ingress controller

--- deployment_tools/create_kubernetes_deployment.sh
+++ deployment_tools/create_kubernetes_deployment.sh
@@ -1,8 +1,14 @@
-kubectl create -f 01_posgres_secret.yaml
-kubectl create -f 02_posgres_volume_claim.yaml
-kubectl create -f 03_postgres_deployment.yaml
-kubectl create -f 04_postgres_cluster-ip-service.yaml
-kubectl create -f 05_backend_deployment.yaml
-kubectl create -f 06_backend_ip-service.yaml
-kubectl create -f 07_frontend_deployment.yaml
-kubectl create -f 08_frontend_ip-service.yaml
+relDir="$(dirname -- "$0"; )";
+cd "$relDir" || exit 1;
+scriptDirectory="$( pwd; )";
+
+kubernetesFolder=$scriptDirectory/../k8s
+
+kubectl create -f $kubernetesFolder/01_posgres_secret.yaml
+kubectl create -f $kubernetesFolder/02_posgres_volume_claim.yaml
+kubectl create -f $kubernetesFolder/03_postgres_deployment.yaml
+kubectl create -f $kubernetesFolder/04_postgres_cluster-ip-service.yaml
+kubectl create -f $kubernetesFolder/05_backend_deployment.yaml
+kubectl create -f $kubernetesFolder/06_backend_ip-service.yaml
+kubectl create -f $kubernetesFolder/07_frontend_deployment.yaml
+kubectl create -f $kubernetesFolder/08_frontend_ip-service.yaml
