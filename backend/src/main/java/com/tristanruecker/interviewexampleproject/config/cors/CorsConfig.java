c09ce854754ef45b3c94bc4e2e4c9df8bc290b02
flexcommits <andersensven.star@gmail.com>
2022-11-20 22:52:09 +0100

    Add cors configuration to project and correct ingress file

---
+++ backend/src/main/java/com/tristanruecker/interviewexampleproject/config/cors/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.tristanruecker.interviewexampleproject.config.cors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.debug("Setup Spring Cors configuration");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "http://interview.test");
+    }
+}
