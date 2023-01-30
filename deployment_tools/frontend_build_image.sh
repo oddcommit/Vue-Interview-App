1af12c383b085d879721b15a25742685fbebdc69
flexcommits <andersensven.star@gmail.com>
2023-01-30 21:11:41 +0100

    Build frontend with pipeline

---
+++ deployment_tools/frontend_build_image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+source ./helper_tools.sh
+
+dockerHubAccount=21321321421441
+scriptDirectory=$(get_relative_path_of_script_directory)
+frontendTagName=interview-example-project-frontend
+frontendReactFolder=$scriptDirectory/../frontend-react
+packageJsonFile=$frontendReactFolder/package.json
+projectFrontendVersion="$(cat "$packageJsonFile" | grep "\"version\"" | grep -Eo "[0-9]+.[0-9]+.[0-9]+")"
+
+create_react_frontend_docker_file() {
+    cd "$frontendReactFolder" || exit 1
+    echo "Prepare frontend-react Dockerfile for production..."
+    echo "Building docker image..."
+    docker build -t $frontendTagName:"$projectFrontendVersion" -t $frontendTagName:latest .
+    echo "Docker image build successful..."
+    echo "Tagging frontend image..."
+    docker tag $frontendTagName:"$projectFrontendVersion" $dockerHubAccount/$frontendTagName:"$projectFrontendVersion"
+    docker tag $frontendTagName:"$projectFrontendVersion" $dockerHubAccount/$frontendTagName:latest
+    echo "Tagging frontend successful"
+    echo "Pushing image to docker hub..."
+#    docker push $dockerHubAccount/$frontendTagName:"$projectFrontendVersion"
+#    docker push $dockerHubAccount/$frontendTagName:latest
+    echo "Push successful"
+    cd "$scriptDirectory" || exit 1
+}
+
+create_react_frontend_docker_file
+
+# End
+echo "Build script end."
